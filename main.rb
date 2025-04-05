d2bacdb9579f5001434fd95165173467782de797
Load A <143745153+load-a@users.noreply.github.com>
2025-04-04 21:46:56 -0400

    Minor testing and character changes

--- main.rb
+++ main.rb
@@ -10,74 +10,11 @@ require_relative 'special'
 require_relative 'setup'
 
 teams = [
-  [CHARACTERS[:alyssa], CHARACTERS[:xia], CHARACTERS[:yumi]], 
-  [CHARACTERS[:barney], CHARACTERS[:tomoe]]
+  [CHARACTERS[:alyssa], CHARACTERS[:xia], CHARACTERS[:yumi], CHARACTERS[:tomoe]], 
+  [CHARACTERS[:isolde], CHARACTERS[:melvin], CHARACTERS[:jamal], CHARACTERS[:ray]]
 ]
 
 match = Game.play_match(*teams)
 
 system 'clear'
 puts match.join("\n")
-
-# players = 
-# order = players.dup
-
-# tally = {
-#   average_rounds: []
-# }
-
-# players.each {|character| tally[character.name] = 0}
-
-# round = 0
-# game_match = 0
-# simulations = 1000
-
-# sample = rand(1..simulations)
-# # sample = simulations + 1
-
-# def status_text(players, width)
-#   side_length = (width - 4) / 2
-#   right = players[0].status.rjust(side_length)
-#   left = players[1].status.ljust(side_length)
-#   '%s vs %s' % [right, left]
-# end
-
-
-# simulations.times do
-#   game_match += 1
-#   round = 0
-
-#   players.each(&:full_reset)
-#   puts "-- Match #{game_match} --".center(120) if game_match == sample
-
-#   loop do
-#     round += 1
-#     order.reverse!
-
-#     puts "~~Round #{round}~~".center(120) if game_match == sample
-#     puts status_text(players, 120) if game_match == sample
-
-#     battle_log = Game.round_of_combat(order[0], order[1])
-#     puts battle_log.join("\n  ") if game_match == sample
-
-#     if players[0].down?
-#       puts "#{players[1].name} wins!" if game_match == sample
-#       tally[players[1].name] += 1
-#       break
-#     elsif players[1].down?
-#       puts "#{players[0].name} wins!" if game_match == sample
-#       tally[players[0].name] += 1
-#       break
-#     end
-#   end
-
-#   tally[:average_rounds] << round
-# end
-
-# tally[:average_rounds] = tally[:average_rounds].sum / tally[:average_rounds].length
-
-# puts "Final Tally:", tally
-
-# Notes:
-# The person to attack first seems to have a slight disadvantage
-
